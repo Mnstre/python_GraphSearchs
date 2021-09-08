e81b3c2cc09d0e66d13b6e938be084a825be7eba
Miguel <gmiguel.503@gmail.com>
2021-09-08 13:55:36 -0500

    1qaz

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+#   Graph Searches Program
+#   Create, Edit and Eliminate Vertex and Edges of a Graph
+#
+#   Mnstre @ September, 2021
+class graphManager:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = []
+        self.gdict = gdict
+    def getVertex(self):
+        return list(self.gdict.keys())
 
+graph = {
+    "a" : ["b", "c"],
+    "b" : ["a", "d"],
+    "c" : ["a", "d"],
+    "d" : ["e"],
+    "e" : ["d"]
+}
+
+g = graphManager(graph)
+print(graph)
+print(g.getVertex())
